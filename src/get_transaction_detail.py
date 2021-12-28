5bbcfa2f2360c93afe62d325a6a52023e2b092fc
watagori <cottongorillla@gmail.com>
2021-12-28 03:56:37 +0000

    fix: exchangfe address

--- src/get_transaction_detail.py
+++ src/get_transaction_detail.py
@@ -7,7 +7,7 @@ from web3 import Web3
 
 # transaction history
 # transafer(waki -> nishi) 0x57932a25175a1184d433be880655d1ee6d1a9e62853723e695f1d38553914462
-# exchange(BNB -> Cake) 0xda28ecfc40181a6dad8b52723035dfba3386d26e
+# exchange(BNB -> Cake) 0x4f8534e85849cb54f0ae4ca0718939ab22de248f64e2e4dc607a76b12f20f109
 # cake max approve 0x36512c7e09e3570dfc53176252678ee9617660550d36f4da797afba6fc55bba6
 # liquidity(BNB + Cake -> LP) 0xb4c3ed5db127089cd1be4b247537d163e74e99e33598348579ffae4f81877834
 # liquidity(LP -> BNB + Cake) 0xdc70901bcb2517a885e41ab9ccb0a739ae73af4b8862a1c46f9ca2ce583b8cd3
