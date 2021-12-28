2ed25718b35092225a4af8f5389a0dc5742a502a
watagori <cottongorillla@gmail.com>
2021-12-28 01:48:54 +0000

    feat: change test address

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@
 import get_transaction_list
 
 TRANSACTION_NUMBER = 10
-TRANSACTION_ADDRESS = "0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F"  # wakimoto-san
-# TRANSACTION_ADDRESS= "0x10b02dC3df82f53c0C453f3639211f769cD36cfe" #panckake only
+# TRANSACTION_ADDRESS = "0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F"  # wakimoto-san
+TRANSACTION_ADDRESS = "0xDa28ecfc40181a6DAD8b52723035DFba3386d26E"  # panckake only
 PLATFORM_NAME = "PancakeSwap"
 
 get_transaction_list.get_transaction_by_address(
