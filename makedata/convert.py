77cbf3eed4618b24ffb3628e34bf8d00284bc8e9
watagori <cottongorillla@gmail.com>
2021-12-31 03:34:33 +0000

    feat: add transaction data

--- makedata/convert.py
+++ makedata/convert.py
@@ -2,6 +2,9 @@
 # from decimal import Decimal
 # from datetime import datetime
 from web3 import Web3
+import certifi
+import requests
+import json
 
 # transaction history
 # transafer(waki -> nishi) 0x57932a25175a1184d433be880655d1ee6d1a9e62853723e695f1d38553914462
@@ -16,14 +19,33 @@ BSC_URL = "https://bsc-dataseed.binance.org/"
 w3 = Web3(Web3.HTTPProvider(BSC_URL))
 
 
-def get_transaction_by_address(transaction_id):
+def get_logs_by_address(transaction_id):
     '''Get transaction by address'''
     bsc_receipt = w3.eth.get_transaction_receipt(transaction_id)
 
     jsondata = Web3.toJSON(bsc_receipt["logs"])
-    jsonfile = open("data/transfer_waki_to_nishi.json", "w", encoding="utf-8")
+    jsonfile = open("data/logs/transfer_waki_to_nishi.json",
+                    "w", encoding="utf-8")
     jsonfile.write(jsondata)
     jsonfile.close()
 
 
-get_transaction_by_address(TRANSACTION_ID)
+get_logs_by_address(TRANSACTION_ID)
+
+
+def get_transaction(transaction_number, address):
+    url = "https://api.bscscan.com/api?module=account&action=txlist&address=" + address \
+        + "&startblock=0&endblock=99999999&page=1&offset=" + str(transaction_number) \
+        + "&sort=desc&apikey=TBY4C25PY394RSWTF6DUXWCHJTYF8Q8H1U"
+    response = requests.get(url, verify=certifi.where())
+    address_content = response.json()
+    details = address_content.get("result")
+
+    for index, transaction in enumerate(details):
+
+        with open("data/transaction/transaction" + str(index) + ".json", "w", encoding="utf-8") as outfile:
+            json.dump(transaction, outfile)
+
+
+get_transaction(
+    5, "0xDa28ecfc40181a6DAD8b52723035DFba3386d26E")
--- tests/test_logs.py
+++ tests/test_logs.py
@@ -3,28 +3,28 @@ from src.logs import Logs
 
 
 def test_approve_01():
-    file = open("data/approve_cake_max.json", "r", encoding="utf-8")
+    file = open("data/logs/approve_cake_max.json", "r", encoding="utf-8")
     jsondata = json.load(file)
     file.close()
     assert Logs().get_transaction_type(jsondata) == "approve"
 
 
 def test_deposit_01():
-    file = open("data/exchange_bnb_to_cake.json", "r", encoding="utf-8")
+    file = open("data/logs/exchange_bnb_to_cake.json", "r", encoding="utf-8")
     jsondata = json.load(file)
     file.close()
     assert Logs().get_transaction_type(jsondata) == "exchange-bnb"
 
 
 def test_exchange_cake_to_bnb_01():
-    file = open("data/exchange_cake_to_bnb.json", "r", encoding="utf-8")
+    file = open("data/logs/exchange_cake_to_bnb.json", "r", encoding="utf-8")
     jsondata = json.load(file)
     file.close()
     assert Logs().get_transaction_type(jsondata) == "exchange"
 
 
 def test_liquidity_bnb_and_cake_to_lp_01():
-    file = open("data/liquidity_bnb_and_cake_to_lp.json",
+    file = open("data/logs/liquidity_bnb_and_cake_to_lp.json",
                 "r", encoding="utf-8")
     jsondata = json.load(file)
     file.close()
@@ -32,7 +32,7 @@ def test_liquidity_bnb_and_cake_to_lp_01():
 
 
 def test_liquidity_lp_to_bnb_and_cake_01():
-    file = open("data/liquidity_lp_to_bnb_and_cake.json",
+    file = open("data/logs/liquidity_lp_to_bnb_and_cake.json",
                 "r", encoding="utf-8")
     jsondata = json.load(file)
     file.close()
