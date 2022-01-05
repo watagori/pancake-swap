46e42eea759dccc25ee2e1b2b9613e8ea217c22d
watagori <cottongorillla@gmail.com>
2022-01-05 09:40:40 +0000

    feat: header to

--- src/header.py
+++ src/header.py
@@ -2,6 +2,7 @@
 from datetime import datetime
 from decimal import Decimal
 
+
 WEI = 1000000000000000000
 
 
@@ -10,7 +11,8 @@ class Header(object):
         self.timestamp = header['timeStamp']
         self.gas_price = header['gasPrice']
         self.gas_used = header['gasUsed']
-        self.fee_from = header['from']
+        self.transaction_from = header['from']
+        self.transaction_to = header['to']
         self.transaction_hash = header['hash']
 
     def get_time(self):
@@ -23,10 +25,14 @@ class Header(object):
             self.gas_price) * Decimal(self.gas_used)/Decimal(WEI))
         return transaction_fee
 
-    def get_fee_from(self):
-        fee_from = self.fee_from
-        return fee_from
+    def get_transaction_from(self):
+        transaction_from = self.transaction_from
+        return transaction_from
 
     def get_transaction_hash(self):
         transaction_hash = self.transaction_hash
         return transaction_hash
+
+    def get_transaction_to(self):
+        transaction_to = self.transaction_to
+        return transaction_to
--- tests/test_header.py
+++ tests/test_header.py
@@ -31,7 +31,7 @@ class TestHeader:
         file.close()
         fee_from = Header(
             header)\
-            .get_fee_from()
+            .get_transaction_from()
         assert fee_from == "0xda28ecfc40181a6dad8b52723035dfba3386d26e"
 
     def test_get_transaction_hash_01(self):
@@ -42,3 +42,19 @@ class TestHeader:
         transaction_hash = Header(header).get_transaction_hash()
         assert transaction_hash ==\
             "0x4f8534e85849cb54f0ae4ca0718939ab22de248f64e2e4dc607a76b12f20f109"
+
+    def test_get_transaction_to_01(self):
+        file = open("data/header/approve.json",
+                    "r", encoding="utf-8")
+        header = json.load(file)
+        file.close()
+        transaction_to = Header(header).get_transaction_to()
+        assert transaction_to == "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82"
+
+    def test_get_transaction_to_02(self):
+        file = open("data/header/exchange_bnb_to_cake.json",
+                    "r", encoding="utf-8")
+        header = json.load(file)
+        file.close()
+        transaction_to = Header(header).get_transaction_to()
+        assert transaction_to == "0x10ed43c718714eb63d5aa57b78b54704e256024e"
